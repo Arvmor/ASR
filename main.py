b5c52deec72fc2b296451a4c1d32e2cb45238acf
Goodzilam <arvin.morawej@gmail.com>
2020-05-04 01:43:59 +0430

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
+# requirements
+# pip install selenium
+# replace your skyroom class URL, username and password
+# move your chromedriver.exe to same path
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
+
 #**************************************************************************
-chromedriver = "chromedriver.exe" # your chrome driver path
+chromedriver = "chromedriver.exe" # your chromedriver.exe path
+driver = webdriver.Chrome(chromedriver)
 #**************************************************************************
 
 #login
-driver = webdriver.Chrome(chromedriver)
 url = "your skyroom link url"
 driver.get(url)
-time.sleep(1)
-time.sleep(1)
+time.sleep(2)
 driver.find_element(By.XPATH, '//*[@id="username"]').send_keys("username")
 driver.find_element(By.XPATH, '//*[@id="password"]').send_keys("password")
 time.sleep(2)
